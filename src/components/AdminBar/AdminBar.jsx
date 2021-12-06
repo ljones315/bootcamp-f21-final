74a6fba0baa03037fc9905eb79dd5685f473b6ea
Yash Gupta <anshya17@gmail.com>
2021-12-06 18:42:26 -0500

    Adding admin features

--- src/components/AdminBar/AdminBar.jsx
+++ src/components/AdminBar/AdminBar.jsx
@@ -1,10 +1,47 @@
+//import { Db } from 'mongodb'
 import {useEffect, useState, React} from 'react'
-//import styles from './AdoptPage.module.css'
+import styles from './AdminBar.module.css'
 
 export default function AdminBar() {
+    const [data, setData] = useState([])
+    const [search, setSearch] = useState("");
+
+    const handleUserInput = (e) => {
+      setSearch(e.target.value);
+    };
+    const handleSubmit = (e) => {
+      console.log("submit button clicked")
+      const newData = data.filter(e => e.catName === search)
+      setData([...newData])
+    };
+    useEffect(() => {
+      let url = "http://localhost:3001/adoptions"
+      fetch(url)
+        .then(response => response.json()) 
+        .then(object => {
+            /*const item = object.map(feature =>
+              <div className={styles.card}>
+                  <li>{feature.adopterName}</li>
+                  <li>{feature.location}</li>
+                  <li>{feature.email}</li>
+              </div>
+              )*/
+              setData(object)
+        })
+    ,[]    
+    })
+
     return (
     <div>
-      <input placeholder="Enter name of cat"/>
+      <h1>Applicants for Adopting Cats</h1>
+      <input placeholder="Enter name of cat" onChange={handleUserInput}/>
+      <button type = "Submit" onClick={handleSubmit}>Submit</button>
+      {data.map(feature =>
+              <div className={styles.card}>
+                  <li>{feature.adopterName}</li>
+                  <li>{feature.location}</li>
+                  <li>{feature.email}</li>
+              </div>)}
     </div>
     )
 }
