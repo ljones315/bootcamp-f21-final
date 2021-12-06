e8c8c93be5ba3390fcb1863476ce839f149adbec
Yash Gupta <anshya17@gmail.com>
2021-12-05 19:36:03 -0500

    remove redundant code

--- src/screens/Index/IndexPage.jsx
+++ src/screens/Index/IndexPage.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import { helloWorld } from "../../actions/General";
 import classes from "./IndexPage.module.css";
 
 const IndexPage = () => {
-  const [payload, setPayload] = React.useState("");
-
-  React.useEffect(() => {
-    // Example how to create page without ssr
-    helloWorld().then((resp) => {
-      setPayload(resp);
-    });
-  }, []);
-
   return (
     <>
       <h2 className={classes.centerText}>Welcome to the Furry Friend Home!</h2>
